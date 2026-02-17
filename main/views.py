9ae57908d02dd18f6d9d6d61fcfdbd715b5878c6
arelaw <arelaw@example.com>
2026-02-17 20:31:25 +0300

    Work in the section information completed

--- main/views.py
+++ main/views.py
@@ -16,3 +16,20 @@ def about(request):
         'text': 'Текст о том какой классный этот интернет магазин.'
     }
     return render(request, 'main/about.html', context)
+
+
+def contacts(request):
+    context = {
+        'title': 'TechStore - Контакты',
+        'content': 'Контакты',
+        'text': 'Здесь должны располагаться контакты нашего магазина.'
+    }
+    return render(request, 'main/contacts.html', context)
+
+def delivery_payment(request):
+    context = {
+        'title': 'TechStore - Доставка и оплата',
+        'content': 'Доставка и оплата',
+        'text': 'Здесь должна располагаться информация о доставке и оплате нашего магазина.'
+    }
+    return render(request, 'main/delivery_payments.html', context)
